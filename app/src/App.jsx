b37d00f8069eb6c638746a96b7784d741b1310cb
Gokul <gokulchandan24@gmail.com>
2024-01-27 13:36:53 +0530

    added other components

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,29 @@
-function App() {
-  return <>hello</>;
-}
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+import Contact from "./components/contact/Contact";
+import Hero from "./components/hero/Hero";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+
+const App = () => {
+  return (
+    <div>
+      Cursor
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">Parallax</section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
+};
 
 export default App;
